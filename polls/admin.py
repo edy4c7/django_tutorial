79dfab8ec9916589574bcbc0e2eed6f10b11951d
edy4c7 <edy4c7@gmail.com>
2019-02-19 21:33:02 +0900

    pep8に対応した。

--- polls/admin.py
+++ polls/admin.py
@@ -2,9 +2,13 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
+# Register your models here.
+
+
 class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
+
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
@@ -16,6 +20,5 @@ class QuestionAdmin(admin.ModelAdmin):
     list_filter = ['pub_date']
     search_fields = ['question_text']
 
-admin.site.register(Question, QuestionAdmin)
 
-# Register your models here.
+admin.site.register(Question, QuestionAdmin)
